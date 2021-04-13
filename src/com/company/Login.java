a2329a7a875d4795204af754df07c5b15e964699
Luana Franca <21898@student.dorset-college.ie>
2021-04-13 13:15:43 +0100

    First Commit

---
+++ src/com/company/Login.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public abstract class Login {
+
+    public String id;
+    public String firstName;
+    public String middleName;
+    public String lastName;
+    public String contactNumber;
+    public String password;
+    public int age;
+
+}
