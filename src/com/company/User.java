c67463c94a134fea9fa69fea6bf763f88e51cd36
Luana Franca <21898@student.dorset-college.ie>
2021-05-04 15:31:30 +0100

    Fourth Commit

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -5,19 +5,26 @@ public abstract class User {
 
     public String firstName;
     public String lastName;
+    public String address;
     public String numberDoc;
     public String emailAddress;
     public String loginId;
     public String password;
+    public String contactNumber;
+    public String collegeName;
 
-    public User(String firstName, String lastName, String numberDoc, String emailAddress, String loginId, String password){
+    public void User(String firstName, String lastName, String address, String numberDoc, String emailAddress, String loginId,
+                     String password, String contactNumber, String collegeName){
 
         this.firstName = firstName;
         this.lastName = lastName;
+        this.address = address;
         this.numberDoc = numberDoc;
         this.emailAddress = emailAddress;
         this.loginId = loginId;
         this.password = password;
+        this.contactNumber = contactNumber;
+        this.collegeName = collegeName;
     }
 
     public User() {
@@ -46,6 +53,18 @@ public abstract class User {
 
     public String getPassword () {
         return password;
+    }
+
+    public String getContactNumber () {
+        return contactNumber;
+    }
+
+    public String getCollegeName() {
+        return collegeName;
+    }
+
+    public String getAddress() {
+        return address;
     }
 }
 
